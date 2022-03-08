5672c225863f4e0d11befb031d69a5a6cf9776ec
MariyBelova <98704442+MariyBelova@users.noreply.github.com>
2022-03-08 23:28:32 +0300

    Update Main.java
    deleting code for homework 16

--- src/Main.java
+++ src/Main.java
@@ -18,25 +18,6 @@ public class Main {
         }
         System.out.println("Количество игроков, которые выбывают:" + numberOfRetiredPlayers);
 
-        boolean p = UtilsHw16.valueSpeed(speedOfPlayer1);
-        if (p) {
-            System.out.println("игрок 1 выбыл");
-        } else {
-            System.out.println("игрок 1 остается в игре");
-        }
-        boolean p1 = UtilsHw16.valueSpeed(speedOfPlayer2);
-        if (p1) {
-            System.out.println("игрок 2 выбыл");
-        } else {
-            System.out.println("игрок 2 остается в игре");
-        }
-        boolean p2 = UtilsHw16.valueSpeed(speedOfPlayer3);
-        if (p2) {
-            System.out.println("игрок 3 выбыл");
-        } else {
-            System.out.println("игрок 3 остается в игре");
-        }
-
         int[] speedOfPlayers = {5, 0, 2};
         System.out.println("Количество выбывших игроков " + Homework17.cnt(speedOfPlayers));
         int[] losers1 = Homework17.loser(speedOfPlayers);
