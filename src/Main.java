4a6edac33c28a561e2a8a7dd25aae4f7837b689b
MariyBelova <98704442+MariyBelova@users.noreply.github.com>
2022-03-08 21:30:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -37,14 +37,7 @@ public class Main {
         } else {
             System.out.println("игрок 3 остается в игре");
         }
-        int[] speedOfPlayers = {5, 0, 2};
-        System.out.println("Количество выбывших игроков " + Homework17.cnt(speedOfPlayers));
-        int[] losers1 = Homework17.loser(speedOfPlayers);
-        int[] winners1 = Homework17.winner(speedOfPlayers);
-        System.out.println("Скорости выбывших игроков:");
-        System.out.println(Arrays.toString(losers1));
-        System.out.println("Скорости не выбывших игроков:");
-        System.out.println(Arrays.toString(winners1));
+        
     }
 
 }
